d64e88a869610c9ac3e4787c8cfa71dfe0a165d8
AnaCarolinaCCH123 <ac521814@gmail.com>
2024-10-08 16:30:23 -0600

    initial

---
+++ Documents/Semestre 3/IoT/Software/JavaScript/app.js
@@ -0,0 +1,15 @@
+const express = requiere('express');
+const app =express();
+const port=3000;
+
+//Middleware para analizar JSON
+app.use(express.jaosn());
+
+//Ruta de ejemplo 
+app.get('/', (req,res) => {
+    res.send('¡Hola mindo');
+});
+
+//Iniciar el servidor
+app.listen(port, () => {}
+)
